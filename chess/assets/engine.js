3b7e6fdef8850de9c9da57beb42b0a6abfc5e8d5
inspiria12 <inspiria12@gmail.com>
2016-02-02 14:28:06 +0530

    chess incomplete

--- chess/assets/engine.js
+++ chess/assets/engine.js
@@ -3,7 +3,7 @@ var chess={
 		var obj = new Object();
 		var html= document.createElement("div");
 		html.setAttribute("class","bishop");
-		html.style.backgroundImage = "url('"+color".svg')";	
+		html.style.backgroundImage = "url('./assets/"+color+".svg')";	 
 		$(html).draggable({ 
 			revert: true,
 			helper: 'clone',
@@ -12,17 +12,17 @@ var chess={
 		obj.html=html;
 		obj.color = color;
 		obj.getOpponents = function(){
-		if(this.color==0){
-			return this.white;
-		}
-		return this.black;
+			if(this.color==0){
+				return this.white;
+			}
+			return this.black;
 		}
 		obj.isValidMove =function(box){
 			var diffX,diffY,m;
 			diffX= box.x-this.box.x;
 			diffY=box.y-this.box.y;
 			m=diffX/diffY;
-			if(m==1 or m==-1){
+			if(m==1 || m==-1){
 				return 1;
 			}
 			return 0;
@@ -42,27 +42,33 @@ var chess={
 			return 0;
 		}
 		return obj;
-	}
-	isValidMove = function(){
+	},
+	isValidMove : function(){
 		var diffX,diffY,m;
 		diffX= box.x-this.box.x;
 		diffY=box.y-this.box.y;
 		m=diffX/diffY;
-		if(m==1 or m==-1){
+		if(m==1 || m==-1){
 			return 1;
 		}
 		return 0;
-	}
+	},
 	Box: function(x,y){
 		var obj = new Object();
 		var html= document.createElement("div");
 		html.setAttribute("class","chess-box");
+		if((x+y)%2==0){
+			html.style.backgroundColor="#333";
+		}else{
+			html.style.backgroundColor="#fff";
+		}
 		$(html).droppable({
   			accept: '.bishop',
 			drop: function(event, ui) {
 				//Get bishop and box********************************
-				if(bishop.isValidMove(box) == 1) {
+				if(bishop.canMakeMove(box) == 1) {
 					$('.ui-draggable-dragging').hide();
+					this.checkForVictory();
 				}
 			} 
 		});
@@ -70,26 +76,70 @@ var chess={
 		obj.y= y;
 		obj.html= html;
 		return obj;
-	}
+	},
 	createBoxs: function(){
 		var board= document.getElementById("chess-board");
-		for(var i=0;i<5;i++){
-			for(var j=0;j<4;j++){
+		for(var i=0;i<4;i++){
+			for(var j=0;j<5;j++){
 				this.boxs[i][j] = this.Box(i,j);
-				board.appendChild(boxs[i][j]);
+				board.appendChild(this.boxs[i][j].html);
 			}
 		}
-	}
+	},
 	createBishops: function(){
+		var board= document.getElementById("chess-board");
 		for(var i=0;i<2;i++){
-			var black[i] = this.Bishop(0);
-			var white[i] = this.Bishop(1);
-			black[i].box=this.boxs[0][2*i+1];
-			white[i].box=this.boxs[4][2*i+1];
+			this.black[i] = this.Bishop(0);
+			this.white[i] = this.Bishop(1);
+			this.black[i].box=this.boxs[(2*i)+1][0];
+			this.white[i].box=this.boxs[(2*i)+1][4];
+			board.appendChild(this.white[i].html);
+			board.appendChild(this.black[i].html);
 		}
-	}
+	},
+	checkForVictory: function(){
+		 for(var i=0;i<2;i++){
+		 	if(!((black[i].box==this.boxs[3][4] || black[i].box==this.boxs[1][4]) && (white[i].box==this.boxs[1][0] || white[i].box==this.boxs[4][2]))){
+		 		return 0;
+		 	}
+		}
+		window.alert("You won!");
+		return 1;
+	},
+	setStyle : function(){
+		var h= (0.75*window.innerHeight);
+		var chessBoard= document.getElementById("chess-board");
+		var chessBoxs= document.getElementsByClassName("chess-box");
+		var bishops = document.getElementsByClassName("bishop");
+		chessBoard.style.height= (h);
+		chessBoard.style.width= (h*1.25);
+		for(var i=0; i<chessBoxs.length;i++){
+			chessBoxs[i].style.height=(h*0.25);
+			chessBoxs[i].style.width=(h*0.25);
+		}
+		for(var i=0; i<4;i++){
+			bishops[i].style.height=(h*0.25);
+			bishops[i].style.width=(h*0.25);
+		}
+		for(var i=0;i<2;i++){
+			this.white[i].html.style.top = (this.white[i].box.x*(h*0.25));
+			this.white[i].html.style.left = (this.white[i].box.y*(h*0.25));
+			this.black[i].html.style.top = (this.black[i].box.x*(h*0.25));
+			this.black[i].html.style.left = (this.black[i].box.y*(h*0.25));
+		}
+	},
+	initArrays: function(){
+		this.boxs= new Array(5);
+		for(var i=0; i<5;i++){
+			this.boxs[i]= new Array(4);
+		};
+		this.white= new Array(2);
+		this.black= new Array(2);
+	},
 	init: function(){
-		createBoard();
-		createBishops();
+		this.initArrays();
+		this.createBoxs();
+		this.createBishops();
+		this.setStyle();
 	}
 }
